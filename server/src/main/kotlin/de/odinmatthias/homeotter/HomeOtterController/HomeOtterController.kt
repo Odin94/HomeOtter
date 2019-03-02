f4a59f601b7042013585a9319f9b4068c648e020
Odin <kammerloher.andreas@gmail.com>
2019-03-02 16:13:01 +0100

    Move backend code to server folder, add Controller

--- server/src/main/kotlin/de/odinmatthias/homeotter/HomeOtterController/HomeOtterController.kt
+++ server/src/main/kotlin/de/odinmatthias/homeotter/HomeOtterController/HomeOtterController.kt
@@ -1,3 +1,11 @@
 package de.odinmatthias.homeotter.HomeOtterController
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController()
+class HomeOtterController {
+
+    @GetMapping("/api/hello", produces = ["application/json"])
+    fun example() = "Hello World!"
+}
